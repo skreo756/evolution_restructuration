14d936749cd73ac0bc0f51571ddc8b703dc11524
skreo756 <34244022+skreo756@users.noreply.github.com>
2018-11-05 13:34:55 +0100

    TP

---
+++ ASTView/src/A.java
@@ -0,0 +1,14 @@
+public class A {
+	
+	public static int l = 1;
+	
+	public static void main (String[] args) {
+
+	for (int i = 1; i < 10 ; i++ ) {
+		l = l*i;
+
+	}
+	System.out.println(l);
+	}
+}
+
